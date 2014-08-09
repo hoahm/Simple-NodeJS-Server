4b7c86bffd1a2655648b65ea9130ea0f7652cc4f
Hoa Hoang <hoahm@nustechnology.com>
2014-08-09 16:48:41 +0700

    Add router module
    Author:    Hoa Hoang <nobi.hoa@gmail.com>

---
+++ router.js
@@ -0,0 +1,5 @@
+function route(pathname){
+  console.log("About route request for " + pathname);
+}
+
+exports.route = route;
--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var http = require("http");
+var url = require("url");
 var port = 8888;
 
-function start(){
+function start(route){
   function onRequest(request, response){
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received.");
+
+    route(pathname);
+
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello Wolrd");
     response.end();
