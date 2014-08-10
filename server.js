e465a22a2039123b4ef2af9235de67a4953b5e77
Hoa Hoang <hoahm@nustechnology.com>
2014-08-10 16:15:17 +0700

    Fix non-blocking operation
    Author:    Hoa Hoang <nobi.hoa@gmail.com>

--- server.js
+++ server.js
@@ -7,12 +7,7 @@ function start(route, handle){
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.");
 
-    var content = route(handle, pathname);
-
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write(content);
-    response.write("Hello Wolrd");
-    response.end();
+    route(handle, pathname, response);
   }
 
   http.createServer(onRequest).listen(port);
